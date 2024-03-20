1553ef889d56f3329f94bcc937313af5cb43aeff
SFYLL <santiagoflood@hotmail.fr>
2024-03-20 02:16:50 +0100

    responses types

---
+++ web/src/seismic/lib/response.types.ts
@@ -0,0 +1,20 @@
+import {  WeierstrassSignatureType } from "starknet";
+
+export interface TradeResponse {
+  commitment: string;
+  signature:  WeierstrassSignatureType;
+}
+
+export function convertToBigInt(obj: any): any {
+  let result: any = Array.isArray(obj) ? [] : {};
+  for (const key in obj) {
+    if (typeof obj[key] === 'object') {
+      result[key] = convertToBigInt(obj[key]);
+    } else if (typeof obj[key] === 'string' && /^\d+$/.test(obj[key])) {
+      result[key] = BigInt(obj[key]);
+    } else {
+      result[key] = obj[key];
+    }
+  }
+  return result;
+}
