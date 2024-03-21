2fd36ef5aa7d7478f1cf8b3f6a830f23fb74ba24
SFYLL <santiagoflood@hotmail.fr>
2024-03-21 19:07:58 +0100

    zkp lib

---
+++ web/src/seismic/zkp/types/ZkpParams.ts
@@ -0,0 +1,12 @@
+export interface ZkpParams {
+    amount_in: string;
+    amount_in_rounded_down: string;
+    reserve_in: string;
+    reserve_out: string;
+    amount_out: string;
+    amount_out_rounded_down: string;
+    salt: string;
+    reserve_in_image: string;
+    reserve_out_image: string;
+}
+
